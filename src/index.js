810842c5fc716ac4076cfb41c1137c80b630d6ce
jkatsaris-afk <jkatsaris@pcsdnv.com>
2026-04-18 18:07:51 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+
+// 🔥 YOUR MASTER CSS
 import "./core/styles/main.css";
+
+// 🔥 ROOT RENDER
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
