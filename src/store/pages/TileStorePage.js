4060c6a0779ea58c9ba9c8f62c4ee2cf9dff7bf5
jkatsaris-afk <jkatsaris@pcsdnv.com>
2026-04-18 19:30:33 -0700

    Update TileStorePage.js

---
+++ src/store/pages/TileStorePage.js
@@ -0,0 +1,14 @@
+import TemplatePage from "../../../templates/TemplatePage";
+
+export default function TileStorePage() {
+  return (
+    <TemplatePage title="Tile Store">
+
+      <div>
+        <h2>Tile Store</h2>
+        <p>This is your first real tile app 👍</p>
+      </div>
+
+    </TemplatePage>
+  );
+}
