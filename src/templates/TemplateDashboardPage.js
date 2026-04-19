b4d5171172d072605f750f22fec4de4496e234ea
jkatsaris-afk <jkatsaris@pcsdnv.com>
2026-04-18 19:23:57 -0700

    Update TemplateDashboardPage.js

--- src/templates/TemplateDashboardPage.js
+++ src/templates/TemplateDashboardPage.js
@@ -1,28 +1,8 @@
-import DockLayout from "../core/layout/DockLayout";
-
 export default function TemplateDashboardPage() {
   return (
-    <DockLayout>
-
-      <div className="card">
-        <div className="title">Oikos Display</div>
-        <div className="sub">Dock + Popup working</div>
-      </div>
-
-      <div className="card">
-        <div className="title">Content Area</div>
-        <div className="sub">
-          This will never overlap the dock
-        </div>
-      </div>
-
-      <div className="card">
-        <div className="title">Next Step</div>
-        <div className="sub">
-          Replace popup with Tile Store
-        </div>
-      </div>
-
-    </DockLayout>
+    <div>
+      <h1>Dashboard</h1>
+      <p>This is your home tile content.</p>
+    </div>
   );
 }
