31b895cf2b3e626b5b149e96fb400b4aadb9214d
jkatsaris-afk <jkatsaris@pcsdnv.com>
2026-04-20 11:10:11 -0700

    Create ForgotPasswordPage.js

---
+++ src/core/pages/ForgotPasswordPage.js
@@ -0,0 +1,8 @@
+export default function ForgotPasswordPage() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h2>Forgot Password</h2>
+      <p>Reset flow coming next.</p>
+    </div>
+  );
+}
