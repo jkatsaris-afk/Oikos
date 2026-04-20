a883c913713e628529d6a2ed4d0540b7e405d23f
jkatsaris-afk <jkatsaris@pcsdnv.com>
2026-04-20 09:01:30 -0700

    Update tileDesign.js
    Remove page

--- src/platforms/church/tiles/tileDesign.js
+++ src/platforms/church/tiles/tileDesign.js
@@ -1,13 +1,1 @@
-import AnnouncementTileDesign from "./announcements/TileDesign";
 
-/**
- * =========================================
- * CHURCH TILE DESIGN REGISTRY
- * =========================================
- * ADD NEW TILES HERE ONLY
- */
-
-export const churchTileDesign = {
-  [AnnouncementTileDesign.id]: AnnouncementTileDesign,
-  [CalendarTileDesign.id]: CalendarTileDesign,
-};
