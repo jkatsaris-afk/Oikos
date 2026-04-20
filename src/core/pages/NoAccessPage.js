2f40625dd87958f6a510c1f7336849fd183982ee
jkatsaris-afk <jkatsaris@pcsdnv.com>
2026-04-20 10:12:52 -0700

    Create NoAccessPage.js
    Created NoAccessPage in core/pages
    
    - Handles permission denial globally
    - Prevents broken navigation for restricted routes

---
+++ src/core/pages/NoAccessPage.js
@@ -0,0 +1,8 @@
+export default function NoAccessPage() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h2>No Access</h2>
+      <p>You do not have permission to view this page.</p>
+    </div>
+  );
+}
