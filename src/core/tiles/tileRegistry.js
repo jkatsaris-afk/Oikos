592227aeafa2cf46ca128abe6ee32d790ea67469
jkatsaris-afk <jkatsaris@pcsdnv.com>
2026-04-19 19:08:18 -0700

    Update tileRegistry.js

--- src/core/tiles/tileRegistry.js
+++ src/core/tiles/tileRegistry.js
@@ -1,9 +1,9 @@
 import CalendarHome from "../../platforms/apps/calendar/CalendarHome";
 import CalendarSettings from "../../platforms/apps/calendar/CalendarSettings";
 import CalendarWidget from "../../platforms/apps/calendar/CalendarWidget";
-import AnnouncementTile from "../../platforms/church/tiles/announcements/AnnouncementTile";
-import AnnouncementSettings from "../../platforms/church/tiles/announcements/AnnouncementSettings";
-import AnnouncementWidget from "../../platforms/church/tiles/announcements/AnnouncementWidget";
+import AnnouncementTile from "../../platforms/church/tiles/announcments/AnnouncementTile";
+import AnnouncementSettings from "../../platforms/church/tiles/announcments/AnnouncementSettings";
+import AnnouncementWidget from "../../platforms/church/tiles/announcments/AnnouncementWidget";
 
 export const tileRegistry = {
   calendar: {
