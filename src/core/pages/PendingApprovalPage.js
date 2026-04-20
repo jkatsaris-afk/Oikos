c61b43aec15803020438b8986fcc68c214a73393
jkatsaris-afk <jkatsaris@pcsdnv.com>
2026-04-20 10:12:32 -0700

    Create PendingApprovalPage.js
    Created PendingApprovalPage in core/pages
    
    - Centralized global system page
    - Used for unapproved users after login

---
+++ src/core/pages/PendingApprovalPage.js
@@ -0,0 +1,8 @@
+export default function PendingApprovalPage() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h2>Pending Approval</h2>
+      <p>Your account is waiting for admin approval.</p>
+    </div>
+  );
+}
