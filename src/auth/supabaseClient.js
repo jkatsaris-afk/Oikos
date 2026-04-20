001f3282898abfe5dbc141e1807471cceefc940e
jkatsaris-afk <jkatsaris@pcsdnv.com>
2026-04-20 11:56:35 -0700

    Update supabaseClient.js
    Fix

--- src/auth/supabaseClient.js
+++ src/auth/supabaseClient.js
@@ -3,8 +3,7 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = "https://bblufmhtmsgqiuryllfa.supabase.co";
 const supabaseAnonKey = "sb_publishable_NLLeqNZzvgoFTLCYdmpRaA_p-yFiqiq";
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
-
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
     persistSession: true,
     autoRefreshToken: true,
