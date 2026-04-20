eb76195aa842c0e59e7d48c98c8c64e0a240256b
jkatsaris-afk <jkatsaris@pcsdnv.com>
2026-04-20 05:35:07 -0700

    Update TilePageTemplate.js
    Page Build

--- src/core/tiles/Template/TilePageTemplate.js
+++ src/core/tiles/Template/TilePageTemplate.js
@@ -1,1 +1,46 @@
+import TilePageLayout from "../../layout/TilePageLayout";
+import { useState } from "react";
+import SettingsModal from "../../settings/SettingsModal";
 
+export default function TilePageTemplate({
+  tileId,
+  title,
+  PageComponent,
+  SettingsComponent,
+  InfoComponent,
+}) {
+  const [openSettings, setOpenSettings] = useState(false);
+  const [openInfo, setOpenInfo] = useState(false);
+
+  return (
+    <>
+      <TilePageLayout
+        title={title}
+        onSettings={() => setOpenSettings(true)}
+        onInfo={() => setOpenInfo(true)}
+      >
+        <PageComponent />
+      </TilePageLayout>
+
+      {/* SETTINGS */}
+      {openSettings && SettingsComponent && (
+        <SettingsModal
+          open={openSettings}
+          onClose={() => setOpenSettings(false)}
+        >
+          <SettingsComponent tileId={tileId} />
+        </SettingsModal>
+      )}
+
+      {/* INFO */}
+      {openInfo && InfoComponent && (
+        <SettingsModal
+          open={openInfo}
+          onClose={() => setOpenInfo(false)}
+        >
+          <InfoComponent tileId={tileId} />
+        </SettingsModal>
+      )}
+    </>
+  );
+}
