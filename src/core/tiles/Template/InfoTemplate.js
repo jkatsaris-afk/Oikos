f8230238a9e98a587597d9d93f684abc83070ad1
jkatsaris-afk <jkatsaris@pcsdnv.com>
2026-04-20 05:36:36 -0700

    Update InfoTemplate.js
    feat: add InfoTemplate for tile info panel
    
    - Standardized info display layout
    - Integrated with modal system
    - Matches settings UI structure

--- src/core/tiles/Template/InfoTemplate.js
+++ src/core/tiles/Template/InfoTemplate.js
@@ -1,1 +1,8 @@
-
+export default function InfoTemplate({ title = "About" }) {
+  return (
+    <div style={{ padding: 16 }}>
+      <h3>{title}</h3>
+      <p>Information about this tile</p>
+    </div>
+  );
+}
