aec05dead0fc7878e4043e8783dc6d3733f6c731
jkatsaris-afk <jkatsaris@pcsdnv.com>
2026-04-20 11:10:38 -0700

    Create ResetPasswordPage.js

---
+++ src/core/pages/ResetPasswordPage.js
@@ -0,0 +1,8 @@
+export default function ResetPasswordPage() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h2>Reset Password</h2>
+      <p>Reset page coming next.</p>
+    </div>
+  );
+}
