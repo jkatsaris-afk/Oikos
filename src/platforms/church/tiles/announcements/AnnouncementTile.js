636aed7e260e0f6a16f3070da8c64d909ac4dae6
jkatsaris-afk <jkatsaris@pcsdnv.com>
2026-04-20 05:55:29 -0700

    Delete src/platforms/church/tiles/announcements/AnnouncementTile.js
    Remove

--- src/platforms/church/tiles/announcements/AnnouncementTile.js
+++
@@ -1,8 +0,0 @@
-export default function AnnouncementTile() {
-  return (
-    <div style={{ padding: 20 }}>
-      <h1>WORKING TILE ✅</h1>
-      <p>If you see this, everything is fixed.</p>
-    </div>
-  );
-}
