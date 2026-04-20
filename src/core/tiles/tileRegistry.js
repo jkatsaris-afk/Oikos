162d71f3023ccab4ef6b8d7037515d73376dde7f
jkatsaris-afk <jkatsaris@pcsdnv.com>
2026-04-19 20:00:32 -0700

    Update tileRegistry.js

--- src/core/tiles/tileRegistry.js
+++ src/core/tiles/tileRegistry.js
@@ -1,8 +1,8 @@
 import { Home, StickyNote, Grid } from "lucide-react";
 
-import AnnouncementTile from "../../../platforms/church/tiles/announcements/AnnouncementTile";
-import AnnouncementSettings from "../../../platforms/church/tiles/announcements/AnnouncementSettings";
-import AnnouncementWidget from "../../../platforms/church/tiles/announcements/AnnouncementWidget";
+import AnnouncementTile from "../../platforms/church/tiles/announcements/AnnouncementTile";
+import AnnouncementSettings from "../../platforms/church/tiles/announcements/AnnouncementSettings";
+import AnnouncementWidget from "../../platforms/church/tiles/announcements/AnnouncementWidget";
 
 export const tileRegistry = {
   home: {
