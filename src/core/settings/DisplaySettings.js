d36bb3e2a2a1061a332a86187568e7026e8a1313
jkatsaris-afk <jkatsaris@pcsdnv.com>
2026-04-19 18:15:25 -0700

    Create DisplaySettings.js

---
+++ src/core/settings/DisplaySettings.js
@@ -0,0 +1,13 @@
+export default function DisplaySettings() {
+  return (
+    <div>
+      <h2>Display Settings</h2>
+
+      <p>Background</p>
+      <p>Widgets</p>
+      <p>Profiles</p>
+      <p>Night Mode</p>
+
+    </div>
+  );
+}
