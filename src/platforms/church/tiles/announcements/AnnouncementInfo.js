f8283337fb33ef22cbfcdb3252ae3c6176ff9e1f
jkatsaris-afk <jkatsaris@pcsdnv.com>
2026-04-20 05:56:22 -0700

    Create AnnouncementInfo.js

---
+++ src/platforms/church/tiles/announcements/AnnouncementInfo.js
@@ -0,0 +1,8 @@
+export default function AnnouncementInfo() {
+  return (
+    <div style={{ padding: 16 }}>
+      <h3>About Announcements</h3>
+      <p>This tile displays important church updates.</p>
+    </div>
+  );
+}
